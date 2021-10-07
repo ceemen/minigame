ce7aefefa2b7407efb1eb86df6db2c69141f8221
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2021-10-07 14:55:45 +0100

    Improved platforms
    Changed script for one way platform collision and platform bounce

---
+++ Assets/Scripts/LavaTower/PlayerBounce.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace LavaTower
+{
+    public class PlayerBounce : MonoBehaviour
+    {
+        [SerializeField] private float jumpHeight;
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.CompareTag("Platform"))
+                return;
+            if (transform.position.y < other.transform.position.y)
+                return;
+            var velocity = _rb.velocity;
+            velocity = new Vector3(
+                velocity.x,
+                jumpHeight,
+                velocity.z);
+            _rb.velocity = velocity;
+        }
+    }
+}
