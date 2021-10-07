6ea5b8b6d3f346c0694a9749f795a86f989ab809
Justinas Grigas <jstn_as@protonmail.com>
2021-10-07 19:06:02 +0100

    updated enemy spawner

--- Assets/Scripts/Frogger/EnemySpawner.cs
+++ Assets/Scripts/Frogger/EnemySpawner.cs
@@ -1,41 +1,23 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
+namespace Frogger
 {
-    List<GameObject> prefabList = new List<GameObject>();
-
-    public GameObject Boulder;
-    public GameObject LargeBoulder;
-    public GameObject Spawner;
-
-    private float spawnRate = 5f;
-    private float nextSpawn = 0f;
-
-    public int whatSpawns;
-
-    // Update is called once per frame
-    void Update()
+    public class EnemySpawner : MonoBehaviour
     {
-        if (Time.time > nextSpawn)
-        {
-            whatSpawns = Random.Range(0, 3);
-            print(whatSpawns);
+        [SerializeField] private List<GameObject> prefabList;
 
-            switch (whatSpawns)
-            {
-                case 1:
-                    Instantiate(LargeBoulder, Spawner.transform.position, Quaternion.identity);
-                    break;
-                case 2:
-                    Instantiate(Boulder, Spawner.transform.position, Quaternion.identity);
-                    break;
-                default:
-                    Instantiate(Boulder, Spawner.transform.position, Quaternion.identity);
-                    break;
-            }
-            nextSpawn = Time.time + spawnRate;
+        private const float SpawnRate = 5f;
+        private float _nextSpawn;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Time.time < _nextSpawn)
+                return;
+            var whatSpawns = Random.Range(0, prefabList.Count - 1);
+            Instantiate(prefabList[whatSpawns], transform.position, Quaternion.Euler(0, 90, 0));
+            _nextSpawn = Time.time + SpawnRate;
         }
     }
 }
