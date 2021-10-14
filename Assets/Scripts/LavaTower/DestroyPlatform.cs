df549b7ec3b487ab5125cdcaa70a644a9512804f
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2021-10-14 13:36:10 +0100

    Fixed issue with moving the platforms when it hits the box collider

--- Assets/Scripts/LavaTower/DestroyPlatform.cs
+++ Assets/Scripts/LavaTower/DestroyPlatform.cs
@@ -5,17 +5,11 @@ namespace LavaTower
     public class DestroyPlatform : MonoBehaviour
     {
         public GameObject player;
-        //public GameObject platformPrefab;
-        
+
         private void OnTriggerEnter(Collider other)
         {
-            //Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (12 + Random.Range(0.2f, 1.0f))), Quaternion.identity);
-            //Destroy(other.gameObject);
-            
             other.gameObject.transform.position = new Vector2(Random.Range(-5.5f, 5.5f),
                 player.transform.position.y + (12 + Random.Range(0.2f, 1.0f)));
-            
-            print("moved");
         }
     }
 }
