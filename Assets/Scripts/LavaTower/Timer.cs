e48e219cfce4af067397da583025e08d882d8b94
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2021-10-16 10:24:56 +0100

    Added timer script and text

---
+++ Assets/Scripts/LavaTower/Timer.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LavaTower
+{
+    public class Timer : MonoBehaviour
+    {
+        public float timeRemaining = 10;
+        public bool timerIsRunning = false;
+        public Text timeText;
+
+        private void Start()
+        {
+            timerIsRunning = true;
+        }
+
+        private void Update()
+        {
+            if (timerIsRunning)
+            {
+                if (timeRemaining > 0)
+                {
+                    timeRemaining -= Time.deltaTime;
+                    DisplayTime(timeRemaining);
+                }
+                else
+                {
+                    Debug.Log("Time has run out!");
+                    timeRemaining = 0;
+                    timerIsRunning = false;
+                }
+            }
+        }
+
+        private void DisplayTime(float timeToDisplay)
+        {
+            timeToDisplay += 1;
+
+            float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+            float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+            timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+}
