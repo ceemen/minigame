bb7e81ab6768b0e273d1712fd9e75b2bde807c38
Chris-OHalliday <78023759+Chris-OHalliday@users.noreply.github.com>
2021-10-21 16:58:35 +0100

    Added new scene for minigame
    Chris

--- Assets/Scripts/UI/Lobby.cs
+++ Assets/Scripts/UI/Lobby.cs
@@ -18,7 +18,7 @@ namespace UI
         public void StartGame()
         {
             inputManager.enabled = false;
-            SceneManager.LoadScene("Scenes/FloorDrop");
+            SceneManager.LoadScene("Scenes/Wallpush");
         }
 
         public void Back()
