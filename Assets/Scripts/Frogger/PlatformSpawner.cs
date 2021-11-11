1c0a2f6f50407f74d4ad55eeedb8b002dc796010
Sean Nichols <78358189+OGreatHippo@users.noreply.github.com>
2021-11-11 16:55:17 +0000

    made game playable, multiple platforms can now spawn, player is made child of log now, boulders slowed down

--- Assets/Scripts/Frogger/PlatformSpawner.cs
+++ Assets/Scripts/Frogger/PlatformSpawner.cs
@@ -5,32 +5,38 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     [SerializeField] private List<GameObject> prefabList;
+
     public GameObject platform;
 
     void Start()
     {
-        var whereSpawn = Random.Range(0, prefabList.Count);
-        var amountSpawn = Random.Range(1, prefabList.Count);
+        int amountSpawn = Random.Range(1, prefabList.Count);
 
-        //for(int i = 0; i < amountSpawn; i++)
-        //{
-        //    i = 
-        //}
-
-        switch (whereSpawn)
+        for (int i = 0; i < amountSpawn; i++)
         {
-            case 1:
-                Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
-                break;
-            case 2:
-                Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
-                break;
-            case 3:
-                Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
-                break;
-            default:
-                Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
-                break;
+            int whereSpawn = Random.Range(0, prefabList.Count);
+
+            switch (whereSpawn)
+            {
+                case 1:
+                    Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
+                    break;
+                case 2:
+                    Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
+                    break;
+                case 3:
+                    Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
+                    break;
+                case 4:
+                    Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
+                    break;
+                case 5:
+                    Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
+                    break;
+                default:
+                    Instantiate(platform, prefabList[whereSpawn].transform.position, prefabList[whereSpawn].transform.rotation);
+                    break;
+            }
         }
     }
 }
