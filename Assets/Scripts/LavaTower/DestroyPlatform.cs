990bc9d083a717ac097a76b2cfa44296e7d3222c
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2021-11-25 16:53:27 +0000

    Changed how platform generation works

--- Assets/Scripts/LavaTower/DestroyPlatform.cs
+++ Assets/Scripts/LavaTower/DestroyPlatform.cs
@@ -4,16 +4,44 @@ namespace LavaTower
 {
     public class DestroyPlatform : MonoBehaviour
     {
-        private void OnTriggerEnter(Collider other)
+        //private void OnTriggerEnter(Collider other)
+        //{
+        //    if (other.CompareTag("Player"))
+        //    {
+        //        return;
+        //    }
+        //    other.gameObject.transform.position = new Vector2(
+        //        Random.Range(-7, 7),
+        //        Mathf.Round(transform.position.y) + 35 
+        //        );
+        //}
+
+        [SerializeField]
+        private GameObject platformPrefab;
+        //[SerializeField]
+        //private GameObject shortPlatformPrefab;
+        //[SerializeField]
+        //private GameObject longPlatformPrefab;
+
+        private int numberOfPlatforms = 200;
+        private int levelWidth = 7;
+        private int minY = 3;
+        private int maxY = 7;
+
+        private void Start()
         {
-            if (other.CompareTag("Player"))
+            Vector3 spawnPosition = new Vector3();
+
+            for (int i = 0; i < numberOfPlatforms; i++)
             {
-                return;
+                spawnPosition.y += Random.Range(minY, maxY);
+                spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+                Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+                //Instantiate(shortPlatformPrefab, spawnPosition, Quaternion.identity);
+                //Instantiate(longPlatformPrefab, spawnPosition, Quaternion.identity);
             }
-            other.gameObject.transform.position = new Vector2(
-                Random.Range(-7, 7),
-                Mathf.Round(transform.position.y) + 35 
-                );
         }
+
+
     }
 }
