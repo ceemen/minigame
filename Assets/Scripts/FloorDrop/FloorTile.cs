37c521538d528e61b7ed7d937b09d1e283b50da7
Justinas Grigas <jstn_as@protonmail.com>
2021-12-01 15:54:24 +0000

    floor tile now triggered at start
    fixed floor tile not triggered at the start of the game
    changed the lava texture to be brighter, the floor tiles can now be
    darker

--- Assets/Scripts/FloorDrop/FloorTile.cs
+++ Assets/Scripts/FloorDrop/FloorTile.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace FloorDrop
 {
     public class FloorTile : MonoBehaviour
     {
+        private const float DistanceThreshold = 0.5f;
         [SerializeField, ColorUsage(true, true)] private Color startColour, endColour;
         [SerializeField] private float timer;
 
@@ -13,6 +15,7 @@ namespace FloorDrop
 
         private float _timeLeft;
         private bool _collided;
+        private Vector2 _position;
 
         private void Awake()
         {
@@ -20,14 +23,36 @@ namespace FloorDrop
             _collider = GetComponent<BoxCollider>();
             _renderer = GetComponent<MeshRenderer>();
             _timeLeft = timer;
+            var position = transform.position;
+            _position = new Vector2(position.x, position.z);
+        }
+
+        private void CheckCollision(Collision other)
+        {
+            // Skip if already collided or not enabled
+            if (_collided || !enabled)
+                return;
+            // Skip if not player
+            if (!other.gameObject.CompareTag("Player"))
+                return;
+            // Skip if too far away
+            var pos = other.gameObject.transform.position;
+            var playerPosition = new Vector2(pos.x, pos.z);
+            if (Mathf.Abs(playerPosition.x - _position.x) > DistanceThreshold ||
+                Mathf.Abs(playerPosition.y - _position.y) > DistanceThreshold)
+                return;
+            // Start breaking
+            _collided = true;
         }
 
         private void OnCollisionEnter(Collision other)
         {
-            // Only collide with players, and when enabled..
-            if (!other.gameObject.CompareTag("Player") || !enabled)
-                return;
-            _collided = true;
+            CheckCollision(other);
+        }
+
+        private void OnCollisionStay(Collision other)
+        {
+            CheckCollision(other);
         }
 
         private void Update()
