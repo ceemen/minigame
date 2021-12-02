106d6595150f74d167c445ca04540bcc707cf7bb
Justinas Grigas <jstn_as@protonmail.com>
2021-12-02 17:12:52 +0000

    added more animations
    changed floor texture

--- Assets/Scripts/GameOver/PlayerSpawner.cs
+++ Assets/Scripts/GameOver/PlayerSpawner.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using CoOp;
+﻿using CoOp;
 using UnityEngine;
 
 namespace GameOver
@@ -14,17 +13,19 @@ namespace GameOver
         protected override void SpawnPlayers(PlayerData[] players)
         {
             var spawnPoint = (players.Length - 1) * Vector3.left;
+            var winner = PlayerManager.GetInstance().GetWinner();
             for (var p = 0; p < players.Length; p++)
             {
                 var newPlayer = CreatePlayer(players[p], p);
                 newPlayer.transform.position = spawnPoint;
                 // Set the animation
                 var dance = 0;
-                if (PlayerManager.GetInstance().GetWinner() == p)
+                if (winner == p)
                     dance = Random.Range(1, danceCount + 1);
                 newPlayer.GetComponentInChildren<Animator>().SetInteger(Dance, dance);
+                // Create the pillar
                 var pillarPosition = spawnPoint + pillarOffset;
-                var newPillar = Instantiate(pillarPrefab, pillarPosition, Quaternion.identity);
+                Instantiate(pillarPrefab, pillarPosition, Quaternion.identity);
                 spawnPoint.x += 2;
             }
         }
