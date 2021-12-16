734a1ca275047f3cc72095d4c13a8214886a05d4
Justinas Grigas <jstn_as@protonmail.com>
2021-12-16 17:11:30 +0000

    added score handling
    to get score working for your minigame, call the RemovePlayer function
    from the PlayerSpawner whenever a player dies, or in the order they've
    won from last to first. The function will automatically update the
    scores once all players have been removed.

--- Assets/Scripts/CoOp/PlayerManager.cs
+++ Assets/Scripts/CoOp/PlayerManager.cs
@@ -16,19 +16,14 @@ namespace CoOp
 
         private readonly List<PlayerData> _players = new List<PlayerData>();
 
-        public static PlayerManager GetInstance()
-        {
-            return _instance;
-        }
-
         public static Color GetPlayerColour(int playerIndex)
         {
             return PlayerColours[playerIndex % PlayerColours.Length];
         }
 
-        public List<PlayerData> GetPlayers()
+        public static List<PlayerData> GetPlayers()
         {
-            return _players;
+            return _instance._players;
         }
 
         private void Awake()
@@ -45,18 +40,28 @@ namespace CoOp
         }
 
         // Returns true if player has been added, false if player was already added.
-        public bool AddPlayer(PlayerInput newPlayer)
+        public static bool AddPlayer(PlayerInput newPlayer)
         {
-            print($"Player joined: {newPlayer.devices.ToArray()[0]} {newPlayer.devices.Count}");
-            var newPlayerData = new PlayerData(newPlayer.currentControlScheme, newPlayer.devices[0]);
-            // Ignore players that have already been added.
-            foreach (var playerData in _players)
+            var newPlayerData = new PlayerData(newPlayer.currentControlScheme, newPlayer.devices.ToArray());
+            // Check if the player has already been added
+            foreach (var playerData in _instance._players)
             {
-                if (playerData == newPlayerData)
+                if (playerData.Equals(newPlayerData))
                     return false;
             }
-            _players.Add(newPlayerData);
+            print($"Player {newPlayer.playerIndex} joined: {newPlayer.currentControlScheme}");
+            _instance._players.Add(newPlayerData);
             return true;
+        }
+
+        public static void UpdateScores(IEnumerable<int> players)
+        {
+            var score = 1;
+            foreach (var player in players)
+            {
+                _instance._players[player].AddScore(score);
+                score++;
+            }
         }
     }
 }
