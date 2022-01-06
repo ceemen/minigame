651ec4758b1f7cee5241cdd22cdeb6698da2f966
Sam Watling <samwatling123@hotmail.co.uk>
2022-01-06 12:56:33 +0000

    Mightve fucked some assets

--- Assets/Scripts/SpeedRunner/LevelGen.cs
+++ Assets/Scripts/SpeedRunner/LevelGen.cs
@@ -9,17 +9,45 @@ public class LevelGen : MonoBehaviour
 
     public int numPlayers = 1;
 
-    [SerializeField] private int gameLength = 300;
+    [SerializeField] private int gameLength = 3;
     [SerializeField] private GameObject[] floatPlatforms;
     [SerializeField] private GameObject[] raisedPlatforms;
 
+    private int platformHeight;
 
     void Start()
     {
-
+        platformHeight = 0;
+        Vector3 spawnPosition = new Vector3();
+        int floatIndex = 0;
+        int raiseIndex = 0;
         for (int i = 0; i < gameLength; i++)
         {
+            spawnPosition.x = i * 12;
+            spawnPosition.y = platformHeight * 2;
+            floatIndex = Random.Range(0, floatPlatforms.Length);
+            raiseIndex = Random.Range(0, raisedPlatforms.Length);
 
+            for (int j = 0; j < numPlayers; j++)
+            {
+                spawnPosition.z = j * 3;
+                if(i==0)
+                    Instantiate(floatPlatforms[0], spawnPosition, Quaternion.identity);
+                if (platformHeight == 0)
+                {
+                    Instantiate(floatPlatforms[floatIndex], spawnPosition, Quaternion.identity);
+                }
+                else if(platformHeight>0)
+                {
+                    Instantiate(raisedPlatforms[raiseIndex], spawnPosition, Quaternion.identity);
+                }
+            }
+
+            platformHeight += Random.Range(-1, 2);
+            while(platformHeight>2||platformHeight<0)
+            {
+                platformHeight += Random.Range(-1, 2);
+            }
         }
     }
 
