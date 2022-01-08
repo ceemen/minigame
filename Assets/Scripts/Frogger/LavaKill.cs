1ec59b724c4b8357859f7ac50ed9a712c994f585
Sean Nichols <w9107026@live.tees.ac.uk>
2022-01-08 13:15:56 +0000

    stuff

--- Assets/Scripts/Frogger/LavaKill.cs
+++ Assets/Scripts/Frogger/LavaKill.cs
@@ -2,16 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Frogger;
+using CoOp;
+using UnityEngine.SceneManagement;
 
 public class LavaKill : MonoBehaviour
 {
-    private PlayerControllerFrogger[] players;
+    //private PlayerControllerFrogger[] players;
+
+    [SerializeField] private GameObject loseText;
+
+    private List<GameObject> players;
+
+    private void Start()
+    {
+        players = new List<GameObject>();
+        loseText = GameObject.Find("Lose Text");
+        players.AddRange(GameObject.FindGameObjectsWithTag("Player"));
+       
+    }
 
     void OnCollisionEnter(Collision collision)
     {
-        for(int i = 0; i < players.Length; i++)
+        players.Remove(collision.gameObject);
+        Destroy(collision.gameObject);
+        if (players.Count <= 0)
         {
-            players[i].enabled = false;
+            loseText.gameObject.SetActive(true);
         }
+        //for(int i = 0; i < players.Count; i++)
+        //{
+        //    players[i].enabled = false;
+
+        //}
+    }
+
+    IEnumerator LevelLoad()
+    {
+        yield return new WaitForSeconds(4.0f);
+        SceneManager.LoadScene("Scenes/Menu");
     }
 }
