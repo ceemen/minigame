8f6712612fe3d41401055ce5712f23784da0aa1f
Miguel Ganhao <w9261681@live.tees.ac.uk>
2022-01-11 02:49:11 +0000

    Menu UI

--- Assets/UI/OptionsMenu.cs
+++ Assets/UI/OptionsMenu.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using System.Linq;
 
 namespace UI
 {
@@ -11,12 +13,55 @@ namespace UI
         [SerializeField] private Canvas mainMenu;
         [SerializeField] private PlayerInputManager inputManager;
 
-        public AudioMixer audioMixer;
+        public TMPro.TMP_Dropdown resolutionDropdown;
 
-        public void SetVolumeMaster(float volume)
+        Resolution[] resolutions;
+
+        void Start()
         {
-            audioMixer.SetFloat("volume master", volume);
+            resolutions = Screen.resolutions;
+
+            resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+
+            int currentResolutionIndex = 0;
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + "x" + resolutions[i].height + " - " + resolutions[i].refreshRate + "Hz";
+                options.Add(option);
+
+                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height && resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+                {
+                    currentResolutionIndex = i;
+                }
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
         }
+
+        public void SetResolution(int resolutionIndex)
+        {
+            Resolution resolution = resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen, resolution.refreshRate);
+        }
+
+        public AudioMixer musicMixer;
+
+        public void SetMusicVolume (float volume)
+        {
+            musicMixer.SetFloat("music volume", volume);
+        }
+
+        public AudioMixer soundsMixer;
+
+        public void SetSoundsVolume(float volume)
+        {
+            soundsMixer.SetFloat("sounds volume", volume);
+        }
+
         public void OptionsBack()
         {
             ChangeCanvas(mainMenu);
